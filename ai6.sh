6d4215101bebce7136e9f4dbff8469d9b4cae0a3
the-gigi <the.gigi@gmail.com>
2025-03-24 12:09:15 -0700

    launch ai6 chainlit as a module too

--- ai6.sh
+++ ai6.sh
@@ -5,12 +5,14 @@ if [[ -z "$VIRTUAL_ENV" || "$VIRTUAL_ENV" != *"ai-six/py/venv" ]]; then
   source py/venv/bin/activate
 fi
 
+echo "virtualenv: ${VIRTUAL_ENV}"
+
 if [[ "$1" == "cli" ]]; then
   python -m py.frontend.cli.ai6
 elif [[ "$1" == "slack" ]]; then
   python -m py.frontend.slack.app
 elif [[ "$1" == "chainlit" ]]; then
-  python py/frontend/chainlit/app.py
+  python -m py.frontend.chainlit.app
 else
   echo 'Usage: ai6.sh <cli | slack | chainlit>'
 fi
