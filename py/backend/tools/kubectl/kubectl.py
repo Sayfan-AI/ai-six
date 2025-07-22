37f1a2b7cba5e17a2c3de852e43d366ce83dffaf
the-gigi <the.gigi@gmail.com>
2025-07-22 07:32:53 -0700

    add aws tool + convert kubectl tool to a CommandTool

--- py/backend/tools/kubectl/kubectl.py
+++ py/backend/tools/kubectl/kubectl.py
@@ -1,27 +1,10 @@
-from backend.object_model import Tool, Parameter
-import sh
-import shlex
+from backend.tools.base.command_tool import CommandTool
 
-class Kubectl(Tool):
+class Kubectl(CommandTool):
     def __init__(self, user: str | None = None):
-        self.user = user
-
-        desc = 'Simple Kubectl tool to interact with Kubernetes clusters.'
+        doc_link = "https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands"
         super().__init__(
-            name='kubectl',
-            description=desc,
-            parameters=[Parameter(name='args', type='string', description='command-line arguments for kubectl')],
-            required={'args'}
+            command_name='kubectl',
+            user=user,
+            doc_link=doc_link
         )
-
-    def run(self, **kwargs):
-        args = shlex.split(kwargs['args'])
-
-        # Decide which user to run as (None means run as the current user)
-        try:
-            if self.user is not None:
-                return sh.sudo('-u', self.user, 'kubectl', *args)
-            else:
-                return sh.kubectl(*args)
-        except Exception as e:
-            raise
